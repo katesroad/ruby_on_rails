379cd6d91a3fc6d0323a963dc829654ed0b3bdb9
Kate <test@gmail.com>
2021-04-30 13:53:02 -0400

    get user list

--- airline/app/controllers/api/v1/users_controller.rb
+++ airline/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::UsersController < ApplicationController
 
   def get_users
+    users = User.all
+    if users
+      render json: users, status: :ok
+    else 
+      render json: {msg: 'user empty'}, status: :unprocessable_entity
+    end
   end
 
   def add_user
     user = User.new(userparams)
     if user.save()
-      render json: user, status: :ok
+      render json: user, status: :created
     else
       begin
       render json: {msg: "Add user failed"}, status: :unprocessable_entity
