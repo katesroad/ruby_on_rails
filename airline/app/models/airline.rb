76fd80f9016789a271b8b82521783bcab3d7beea
Kate <test@gmail.com>
2021-04-30 12:24:56 -0400

    add MongoDB ODM

---
+++ airline/app/models/airline.rb
@@ -0,0 +1,19 @@
+class Airline
+  include Mongoid::Document
+  field :name, type: String
+  field :image_url, type: String
+  field :slug, type: String
+
+  has_many :reviews, dependent: :destroy
+
+  before_create :slugify
+
+  def slugify
+    self.slug = slug.parameterize 
+  end
+
+  def  avg_score
+    reviews.average(:score).round(2).to_f
+  end
+
+end
---
+++ airline/app/models/review.rb
@@ -0,0 +1,10 @@
+class Review
+  include Mongoid::Document
+  field :title, type: String
+  field :description, type: String
+  field :score, type: Integer
+  field :airline, type: String
+
+  belongs_to: airline
+  
+end
